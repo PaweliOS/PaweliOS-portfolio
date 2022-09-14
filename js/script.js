b3066159b4ad95b3a23d76491f8cebb5a89722c2
PaweliOS <mirrorpawel.pabisiak@gmail.com>
2022-09-15 00:58:23 +0200

    The main page reorganised and done (without media queries), js btns gotten

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const programmingBtn = document.querySelector('.section-one__programming-btn')
+const photosBtn = document.querySelector('.section-one__photos-btn')
+const hobbyBtn = document.querySelector('.section-one__hobby-btn')
+const aboutBtn = document.querySelector('.section-one__about-btn')
+
+
