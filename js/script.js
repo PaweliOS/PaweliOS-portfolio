cac484693786cda9f40c700a6eac0d6081b32b79
PaweliOS <mirrorpawel.pabisiak@gmail.com>
2022-09-23 15:14:21 +0200

    added page number three, pageNumer counter moved at the end of his function

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ const sectionOne = document.querySelector('.section-one')
 const sectionOneDetails = document.querySelector('.section-one-details')
 const pageOne = document.querySelector('.section-one-details-page-one')
 const pageTwo = document.querySelector('.section-one-details-page-two')
-
+const pageThree = document.querySelector('.section-one-details-page-three')
 
 const sectionTwo = document.querySelector('.section-two')
 const sectionThree = document.querySelector('.section-three')
@@ -27,7 +27,6 @@ let pageNumber = 0
 const goToSectionOneDetails = () => {
     sectionNumber = 1
     pageNumber +=1
-    // sectionOne.classList.add('inactive-section-one')
     programmingBtn.classList.add('inactive')
     sectionTwo.classList.add('inactive')
     sectionThree.classList.add('inactive')
@@ -44,16 +43,7 @@ const goToSectionOneDetails = () => {
 
 
 const goBack = () => {
-    // if ((sectionNumber = 1) && (pageNumber = 1)) {
-    //     sectionOneDetails.classList.add('inactive')
-    //     programmingBtn.classList.remove('inactive')
-    //     sectionTwo.classList.remove('inactive')
-    //     sectionThree.classList.remove('inactive')
-    //     sectionFour.classList.remove('inactive')
-    // }
-    
-    
-    if ((sectionNumber == 1) && (pageNumber == 1)) {
+    if ((sectionNumber == 1) && (pageNumber == 0)) {
         programmingBtn.classList.remove('inactive')
         sectionTwo.classList.remove('inactive')
         sectionThree.classList.remove('inactive')
@@ -66,7 +56,7 @@ const goBack = () => {
         console.log('Robie 1 i 1 Next -> '+sectionNumber+', page: '+pageNumber)
     }
 
-    if ((sectionNumber == 1) && (pageNumber == 2)) {
+    if ((sectionNumber == 1) && (pageNumber == 1)) {
         console.log('SPR: Next -> '+sectionNumber+', page: '+pageNumber)
         pageTwo.classList.add('inactive')
         pageOne.classList.remove('inactive')
@@ -75,13 +65,25 @@ const goBack = () => {
         // pageNumber-=1
         console.log('Robie 1 i 2 Next -> '+sectionNumber+', page: '+pageNumber)
     }
+
+    if ((sectionNumber == 1) && (pageNumber == 2)) {
+        console.log('SPR: Next -> '+sectionNumber+', page: '+pageNumber)
+        pageThree.classList.add('inactive')
+        pageTwo.classList.remove('inactive')
+        // backBtn.classList.add('inactive')
+        // sectionNumber = 1
+        // pageNumber-=1
+        // console.log('Robie 1 i 2 Next -> '+sectionNumber+', page: '+pageNumber)
+    }
+
+
+
     sectionNumber = 1
     pageNumber-=1
 
 
 }
 const goMain = () => {
-    // if ((sectionNumber = 1) && (pageNumber = 1)) {
         pageOne.classList.add('inactive')
         pageTwo.classList.add('inactive')
         sectionOneDetails.classList.add('inactive')
@@ -91,64 +93,35 @@ const goMain = () => {
         sectionFour.classList.remove('inactive')
         sectionNumber = 1
         pageNumber = 0
-    
         console.log('Next -> '+sectionNumber+', page: '+pageNumber)
-    // }
 }
 
 const goNext = () => {
+    // sectionNumber = 1
+    // pageNumber += 1
+    console.log('Next -> '+sectionNumber+', page: '+pageNumber)
+    backBtn.classList.remove('inactive')
+   
+    if ((sectionNumber == 1) && (pageNumber == 1)) {
+        pageOne.classList.add('inactive')
+        pageTwo.classList.remove('inactive') 
+    }
+
+    if ((sectionNumber == 1) && (pageNumber == 2)) {
+        pageTwo.classList.add('inactive')
+        pageThree.classList.remove('inactive') 
+        // zabezpieczam dalsze zwiększanie numeru pageNumber:
+        pageNumber -=1
+    }
     sectionNumber = 1
     pageNumber += 1
-    console.log('Next -> '+sectionNumber+', page: '+pageNumber)
-   
-    if ((sectionNumber == 1) && (pageNumber == 2)) {
-        pageOne.classList.add('inactive')
-        pageTwo.classList.remove('inactive')
-        
-    }
-    
-    backBtn.classList.remove('inactive')
-
-
 
 }
-
-
 const goToSectionThree = () => {
     sectionOne.classList.add('inactive-section-one')
     sectionThree.classList.remove('inactive')
 }
-// const goToSectionFour = () => {
-//     sectionOne.classList.add('inactive-section-one')
-//     sectionFour.classList.remove('inactive')
-// }
 
-
-
-// let sectionOneBtnName
-
-// const switchDisplaySection = () => {
-//     sectionOne.classList.toggle('active')
-// }
-
-// const hideSectionOne = () => {
-//     sectionOne.classList.toggle('inactive-section-one')
-// }
-
-// const sectionOneBtnsListenerSettings = () => {
-//     sectionOneBtns.forEach((btn) => {
-//         btn.addEventListener('click', checkSectionOneBtn)
-//     })
-// }
-
-// const checkSectionOneBtn = (e) => {
-//     if (e.target.classList.consist('.section-one__programming')) {
-//         sectionTwo.classList.remove('.inactive')
-//     }
-
-// }
-
-// document.addEventListener('DOMContentLoaded', sectionOneBtnsListenerSettings);
 programmingBtn.addEventListener('click', goToSectionOneDetails);
 backBtn.addEventListener('click', goBack)
 mainBtn.addEventListener('click', goMain)
